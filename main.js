3f86eaa4b95dced9f9fffe516abebdecf541fbed
ItsDivija <142112600+ItsDivija@users.noreply.github.com>
2024-07-15 19:11:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Espresso" , "Americano" , "Latte" , "Iced Coffee" , "Frappe"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
